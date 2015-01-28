702f6e7869de90a52a8fd2310999f966ef4c8c62
garciadanny <dannygarcia.me@gmail.com>
2015-01-27 17:40:17 -0700

    Rails Router automatically triggers #matches?

--- app/constraints/api_constraint.rb
+++ app/constraints/api_constraint.rb
@@ -5,6 +5,9 @@ class ApiConstraint
     @version = options.fetch( :version )
   end
 
+  # The Rails router will automatically trigger this method.
+  # If it returns `true` it'll use the routes specified within
+  # the scope. 
   def matches? request
     request
       .headers
